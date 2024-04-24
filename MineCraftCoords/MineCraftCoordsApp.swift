e1c76d75184e79b7cfe98d067cdcbc81a8980a9b
KrashVI <ss0848041@otc.edu>
2024-04-24 09:27:58 -0500

    Initial Commit
    Project start

---
+++ MineCraftCoords/MineCraftCoordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MineCraftCoordsApp.swift
+//  MineCraftCoords
+//
+//  Created by SPRING, SEAN on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct MineCraftCoordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
