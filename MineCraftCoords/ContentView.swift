57fe38fd25510c901b0c2c9d2433b1f908a813db
KrashVI <ss0848041@otc.edu>
2024-04-24 10:14:10 -0500

    Added banner and notes
    Played with stacks

--- MineCraftCoords/ContentView.swift
+++ MineCraftCoords/ContentView.swift
@@ -6,19 +6,52 @@
 //
 
 import SwiftUI
+import SwiftData
+
+struct World{
+    public var overWorld: [OverWorldCoord]
+    public var theNether: [NetherCoord]
+    public var theEnd: [TheEndCoord]
+}
+
+struct OverWorldCoord{
+    public var x: Double
+    public var y: Double
+    public var z: Double
+    public var name: String
+}
+
+struct NetherCoord{
+    public var x: Double
+    public var y: Double
+    public var z: Double
+    public var name: String
+}
+
+struct TheEndCoord{
+    public var x: Double
+    public var y: Double
+    public var z: Double
+    public var name: String
+}
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ZStack{
+                Image("minecraft-bedrock-logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+            Text("Hit Me")
+            Spacer()
         }
-        .padding()
     }
 }
-
+// We need a CSV file for saved worlds
+// Nether to OverWorld and OverWorld to Nether converter needs to be a function (1:8 and 8:1) "Portal Estimator"?
+// Add a plus button for adding a new world on main menu, and adding a new coordinate on the subsequent inner worlds
+// Delete function for World and Modify/Delete for inner world coordinates
 #Preview {
     ContentView()
 }
